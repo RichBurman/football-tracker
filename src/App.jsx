55871c9aa816cf78fd4a90ba84d9331073082796
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-07-20 13:02:29 +0100

    refactor App component to use MainLayout and add routing for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import MainLayout from './layouts/MainLayout';
+
+import Home from './pages/Home';
+import ManchesterUnited from './pages/ManchesterUnited';
+import Fixtures from './pages/Fixtures';
+import Players from './pages/Players';
+import PlayerProfile from './pages/PlayerProfile';
+import NotFound from './pages/NotFound';
+
 function App() {
-
   return (
-    <div>
-      <h1>
-        Premier Tracker
-      </h1>
-    </div>
-  )
-
+    <BrowserRouter>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/manchester-united" element={<ManchesterUnited />} />
+          <Route path="/fixtures" element={<Fixtures />} />
+          <Route path="/players" element={<Players />} />
+          <Route path="/players/:id" element={<PlayerProfile />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </MainLayout>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
