c8fc09fc50bea359592ad601e7a5d92dbd91c7d8
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-07-20 12:47:45 +0100

    add NavBar component with navigation links

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+function NavBar() {
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/manchester-united">Manchester United</Link>
+            <Link to="/fixtures">Fixtures</Link>
+            <Link to="/players">Players</Link>
+            <Link to="/not-found">Not Found</Link>
+        </nav>
+    )
+}
+
+export default NavBar;
